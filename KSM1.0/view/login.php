5be73ef0f401684d42e6a44f4e231309b107eba5
nabeel <mohamednabeel0170@gmail.com>
2023-08-18 03:47:30 +0530

    Desgined the login page

---
+++ KSM1.0/view/login.php
@@ -0,0 +1,67 @@
+
+<html>
+  <head>
+    <title>Restaurant Management System</title>
+    <link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.min.css">
+  </head>
+  <body style="background-color:rgb(82, 82, 84)">
+      <div class="container"  style="background-color: rgb(239, 239, 242);height:100%;">
+          <div class="row">
+              <div><h1 align="center">Restaurant Management System</h1></div>
+          </div>
+          <hr>
+          <div class="row">
+               <div class="col-md-6 col-md-offset-3">
+              <div class="row">
+                  <div class="col-md-6 col-md-offset-3" id="alert-msg">&nbsp;</div>
+              </div>
+          <form action="" method="">
+              <div class="panel" style="height:500px;box-shadow:10px 10px 10px #888888;">
+                  <div class="panel-body">
+                      <div class="row" style="height:100px;">
+                          <h2 align="center">Login</h2>
+                      </div>
+                      <div class="row">
+                          <div  class="col-md-8 col-md-offset-2">
+                              <div class="input-group" ">
+                                  <span class="input-group-addon">
+                                      <i class="glyphicon glyphicon-user"></i>
+                                  </span>
+                                  <input type="email" placeholder="Username"class="form-control" style="height: 40px" id="username" name="username"></input>
+                              </div>
+                          </div>
+                      </div>
+                      <div  class="row">
+                                       <div class="col-md-12">&nbsp;</div>
+                      </div>
+                      <div class="row">
+                          <div  class="col-md-8 col-md-offset-2">
+                              <div class="input-group" ">
+                                  <span class="input-group-addon">
+                                      <i class="glyphicon glyphicon-lock"></i>
+                                  </span>
+                                  <input type="password" placeholder="Password"class="form-control" style="height: 40px" id="password" name="password"></input>
+                              </div>
+                          </div>
+                      </div>
+                          <div class="row">
+                                      <div class="col-md-12">&nbsp;</div>
+                          </div>
+                           <div class="row">
+                                        <div class="col-md-8 col-md-offset-2">
+                                            <button type="submit" class="btn btn-success btn-block">
+                                                Login
+                                            </button>
+                                        </div>
+                           </div>
+                      </div>
+                  </div>
+             
+          </form>
+          </div>
+          </div>
+          <hr>
+      </div>
+  </body>
+  <script type="text/javascript" src="../bootstrap/js/bootstrap.min.js"></script>
+</html>

9bef941c8e77897cf3c714c166d57b4af9e13748
nabeel <mohamednabeel0170@gmail.com>
2023-08-19 02:47:43 +0530

    added the admin page

---
+++ KSM1.0/view/admin.php
@@ -0,0 +1,9 @@
+<html>
+  <head>
+    <title>Restaurant Management System</title>
+    <link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.min.css">
+  </head>
+  <body>
+  </body>
+  <script type="text/javascript" src="../bootstrap/js/bootstrap.min.js"></script>
+</html>
